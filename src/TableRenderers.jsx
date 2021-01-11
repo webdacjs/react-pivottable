98a51a9d97181332ae14c10e1fe6b2be434ad468
Juan Convers <me@jco.im>
2021-01-11 07:19:57 +0100

    Moving the non-table renderers to a separate module.

--- src/TableRenderers.jsx
+++ src/TableRenderers.jsx
@@ -7,6 +7,8 @@ import {
   redColorScaleGenerator,
   getHeatmapColors,
 } from './TableUtils';
+import TSVExportRenderer from './TsvRenderer'
+import RawExportRenderer from './RawRenderer'
 
 function makeRenderer(opts = {}) {
   class TableRenderer extends React.PureComponent {
@@ -361,66 +363,6 @@ function makeRenderer(opts = {}) {
   TableRenderer.propTypes.tableOptions = PropTypes.object;
   return TableRenderer;
 }
-
-class TSVExportRenderer extends React.PureComponent {
-  render() {
-    const pivotData = new PivotData(this.props);
-    const rowKeys = pivotData.getRowKeys();
-    const colKeys = pivotData.getColKeys();
-    if (rowKeys.length === 0) {
-      rowKeys.push([]);
-    }
-    if (colKeys.length === 0) {
-      colKeys.push([]);
-    }
-
-    const headerRow = pivotData.props.rows.map(r => r);
-    if (colKeys.length === 1 && colKeys[0].length === 0) {
-      headerRow.push(this.props.aggregatorName);
-    } else {
-      colKeys.map(c => headerRow.push(c.join('-')));
-    }
-
-    const result = rowKeys.map(r => {
-      const row = r.map(x => x);
-      colKeys.map(c => {
-        const v = pivotData.getAggregator(r, c).value();
-        row.push(v ? v : '');
-      });
-      return row;
-    });
-
-    result.unshift(headerRow);
-
-    return (
-      <textarea
-        value={result.map(r => r.join('\t')).join('\n')}
-        style={{width: window.innerWidth / 2, height: window.innerHeight / 2}}
-        readOnly={true}
-      />
-    );
-  }
-}
-
-TSVExportRenderer.defaultProps = PivotData.defaultProps;
-TSVExportRenderer.propTypes = PivotData.propTypes;
-
-class RawExportRenderer extends React.PureComponent {
-  render() {
-    const pivotData = new PivotData(this.props);
-
-    return (
-      <textarea
-        value={JSON.stringify(pivotData, null, 4)} // eslint-disable-line
-        style={{width: window.innerWidth / 2, height: window.innerHeight / 2}}
-        readOnly={true}
-      />
-    );
-  }
-}
-
-RawExportRenderer.defaultProps = PivotData.defaultProps;
-RawExportRenderer.propTypes = PivotData.propTypes;
 
 export default {
   Table: makeRenderer(),
---
+++ src/TsvRenderer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {PivotData} from './Utilities';
+
+class TSVExportRenderer extends React.PureComponent {
+    render() {
+      const pivotData = new PivotData(this.props);
+      const rowKeys = pivotData.getRowKeys();
+      const colKeys = pivotData.getColKeys();
+      if (rowKeys.length === 0) {
+        rowKeys.push([]);
+      }
+      if (colKeys.length === 0) {
+        colKeys.push([]);
+      }
+  
+      const headerRow = pivotData.props.rows.map(r => r);
+      if (colKeys.length === 1 && colKeys[0].length === 0) {
+        headerRow.push(this.props.aggregatorName);
+      } else {
+        colKeys.map(c => headerRow.push(c.join('-')));
+      }
+  
+      const result = rowKeys.map(r => {
+        const row = r.map(x => x);
+        colKeys.map(c => {
+          const v = pivotData.getAggregator(r, c).value();
+          row.push(v ? v : '');
+        });
+        return row;
+      });
+  
+      result.unshift(headerRow);
+  
+      return (
+        <textarea
+          value={result.map(r => r.join('\t')).join('\n')}
+          style={{width: window.innerWidth / 2, height: window.innerHeight / 2}}
+          readOnly={true}
+        />
+      );
+    }
+  }
+  
+  TSVExportRenderer.defaultProps = PivotData.defaultProps;
+  TSVExportRenderer.propTypes = PivotData.propTypes;
+
+  export default TSVExportRenderer
