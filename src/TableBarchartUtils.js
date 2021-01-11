12f55bb233ba3f1f286ff7bbccc117b558696702
Juan Convers <me@jco.im>
2021-01-11 18:27:20 +0100

    Adding function to get min value as well (for negative values)

--- src/TableBarchartUtils.js
+++ src/TableBarchartUtils.js
@@ -1,13 +1,18 @@
-// Function to get the maximum value for each one fhe vals (used to calculate the bar widths).
-export function getMaxValsAttrs(rowTotals, vals) {
-  const excludeKeys = ['push', 'value', 'format', 'numInputs'];
-  const totalRowsValsAttr = Object.keys(rowTotals)
+const excludeKeys = ['push', 'value', 'format', 'numInputs'];
+
+function getTotalRowsValsAttr(rowTotals) {
+  return Object.keys(rowTotals)
     .map(row =>
       Object.keys(rowTotals[row])
         .filter(x => !excludeKeys.includes(x))
         .map(y => rowTotals[row][y])
     )
     .flat();
+}
+
+// Function to get the maximum value for each one fhe vals (used to calculate the bar widths).
+export function getMaxValsAttrs(rowTotals, vals) {
+  const totalRowsValsAttr = getTotalRowsValsAttr(rowTotals);
   const maxValsAttrs = vals.reduce((obj, val) => {
     obj[val] = Math.max(
       ...totalRowsValsAttr
@@ -18,4 +23,19 @@ export function getMaxValsAttrs(rowTotals, vals) {
     return obj;
   }, {});
   return maxValsAttrs;
+}
+
+// Function to get the minimum value for each one fhe vals (used to calculate the bar widths).
+export function getMinValsAttrs(rowTotals, vals) {
+  const totalRowsValsAttr = getTotalRowsValsAttr(rowTotals);
+  const minValsAttrs = vals.reduce((obj, val) => {
+    obj[val] = Math.min(
+      ...totalRowsValsAttr
+        .map(x => x[val])
+        .map(x => parseFloat(x))
+        .filter(Number)
+    );
+    return obj;
+  }, {});
+  return minValsAttrs;
 }
