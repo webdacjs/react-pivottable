755dbd4453c67d4d84d7f5cba9fef88d43a9113f
Juan Convers <me@jco.im>
2021-01-13 16:36:58 +0100

    Add className to the legend span

--- src/TableBarchartRenderer.jsx
+++ src/TableBarchartRenderer.jsx
@@ -168,7 +168,7 @@ class TableBarchartRenderer extends React.PureComponent {
                         className={`bar bar${i + 1}`}
                         style={{width: '10px'}}
                       />
-                      <span key={`attHead${i}`} style={{marginLeft: '4px'}}>
+                      <span key={`attHead${i}`} className="bar-chart-bar-label-span">
                         {`${valsLegend && valsLegend[i] ? valsLegend[i] : x}`}
                       </span>
                     </div>
