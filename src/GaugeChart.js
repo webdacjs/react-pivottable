136a8ed5570ef6f1d3edbb2236e2928f286b2777
Juan Convers <me@jco.im>
2021-03-27 19:23:35 +0100

    Adding first implementation of the GaugeChart using D3

---
+++ src/GaugeChart.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {useD3} from './d3hook.js';
+
+function GaugeChart({
+  dataElement,
+  maxValue,
+  minValue,
+  viewPortWidth,
+  dimensions,
+  colors,
+  height,
+}) {
+
+  const randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
+
+  const getWidth = val => (val * viewPortWidth) / maxValue;
+
+  const values = dimensions.map(x => dataElement[x]);
+
+  function getAdustedX(val) {
+    if (!minValue || minValue > 0) {
+      return val;
+    }
+    if (values.includes(minValue)) {
+      return val;
+    }
+    return val + Math.abs(minValue);
+  }
+
+  const chartHeight = height || 30;
+  const yOffset = (chartHeight / 3) / 2.3;
+  const innerheight = (chartHeight / 3) * 2;
+
+  const chartColors = colors || ['#4e79a7', '#e05759', '#f28e2c'];
+  const builtDataObject = dimensions.map((x, i) => ({
+    dimension: x,
+    y: i === 0 ? 0 : yOffset,
+    width: Math.abs(getWidth(dataElement[x])),
+    height: i === 0 ? chartHeight : innerheight,
+    color: chartColors[i] || randomColor(),
+  }));
+  const widths = builtDataObject.map(x => x.width);
+  const builtDataObjectWithX = builtDataObject.map((item, index) =>
+    Object.assign(item, {
+      x:
+        index <= 1
+          ? getAdustedX(0, item)
+          : getAdustedX(
+              widths.slice(1, index).reduce((a, b) => a + b, 0),
+              item
+            ),
+    })
+  );
+
+  const ref = useD3(
+    svg => {
+      svg
+        .selectAll('rect')
+        .data(builtDataObjectWithX)
+        .enter()
+        .append('rect')
+        .attr('width', d => d.width)
+        .attr('x', d => d.x)
+        .attr('fill', d => d.color)
+        .attr('y', d => d.y)
+        .attr('height', d => d.height);
+    },
+    [dataElement]
+  );
+
+  return (
+    <svg
+      ref={ref}
+      style={{
+        height: chartHeight,
+        width: '100%',
+        marginTop: '0px',
+        marginRight: '0px',
+        marginLeft: '0px',
+      }}
+    ></svg>
+  );
+}
+
+export default GaugeChart;
