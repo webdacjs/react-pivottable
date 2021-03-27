24ac21727c8d2b8d1b701f25fbe7ce1c96b5e945
Juan Convers <me@jco.im>
2021-03-27 22:25:50 +0100

    Adding separate popover component.

---
+++ src/PopOverComponent.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import Popover from 'react-popover';
+
+export default function PopOverComponent({
+  showPopOver,
+  popOverFormatter,
+  rowkey,
+  originalValues,
+  valsAttrs,
+  rowAttrs,
+  children,
+}) {
+  const [hovered, setHovered] = useState();
+
+  const formatPopOverValue = val =>
+    popOverFormatter ? popOverFormatter(val) : val;
+
+  const popOverKeys = [...rowAttrs, ...valsAttrs];
+  const popOverValues = [
+    ...rowkey,
+    ...originalValues.map(x => formatPopOverValue(x)),
+  ];
+
+  const getPopOver = () => (
+    <div className="popoverBox">
+      <table className="popOverBox-table">
+        <tbody>
+          {popOverKeys.map((key, i) => (
+            <tr key={`tr-${i}`}>
+              <td className="popOverBox-table-cell" key={`tdk-${i}`}>
+                {key}:
+              </td>
+              <td className="popOverBox-table-cell" key={`tdv-${i}`}>
+                <b>{popOverValues[i]}</b>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+
+  return (
+    <Popover
+      isOpen={showPopOver ? hovered : false}
+      preferPlace={'below'}
+      body={getPopOver()}
+    >
+      <div onMouseOver={() => setHovered(true)} onMouseOut={() => setHovered()}>
+        {children}
+      </div>
+    </Popover>
+  );
+}
