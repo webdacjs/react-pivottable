1c6e3faeb363cf2bd8f10212c3bde8391a2a1728
Juan Convers <me@jco.im>
2021-03-29 19:47:52 +0200

    Switching to percentage to get width.

--- src/GaugeChartComponent.js
+++ src/GaugeChartComponent.js
@@ -5,7 +5,6 @@ function GaugeChartComponent({
   dataElement,
   maxValue,
   minValue,
-  viewPortWidth,
   dimensions,
   colors,
   height,
@@ -17,7 +16,7 @@ function GaugeChartComponent({
 
   const randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
 
-  const getWidth = val => (val * viewPortWidth) / maxValue;
+  const getWidth = val => (val * 100) / maxValue;
 
   const values = dimensions.map(x => dataElement[x]);
 
@@ -40,7 +39,7 @@ function GaugeChartComponent({
     dimension: x,
     y: i === 0 ? 0 : yOffset,
     text: `${Math.round(dataElement[x])}${suffix}`,
-    width: Math.abs(getWidth(dataElement[x])),
+    width: getWidth(dataElement[x]),
     height: i === 0 ? chartHeight : innerheight,
     color: chartColors[i] || randomColor(),
   }));
