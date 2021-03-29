fd219ca199519e00518901c88e54ce20909b5401
Juan Convers <me@jco.im>
2021-03-29 11:45:01 +0200

    Fixing the D3 Header component.

---
+++ src/D3HeaderComponent.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import * as d3 from 'd3';
+import {useD3} from './d3hook.js';
+
+function D3HeaderComponent({
+  viewPortWidth,
+  height,
+  legendValues,
+  maxValue,
+  minValue
+}) {
+
+  function transformValue(val) {
+      if (val === '') {
+          return minValue
+      }
+      if (val.includes('k')) {
+          return parseFloat(val.replace('k', '')) * 1000
+      }
+      if (val.includes('M')) {
+        return parseFloat(val.replace('M', '')) * 1000000
+      }
+      return parseFloat(val)
+  }
+
+  const data = legendValues.map(x => transformValue(x))
+  data.push(maxValue)
+
+  const scale = d3.scaleLinear()
+    .domain([d3.min(data), d3.max(data)])
+    .range([0, viewPortWidth]);
+
+  const xAxis = d3.axisBottom().scale(scale);
+  console.log(xAxis)
+
+  const ref = useD3(
+    svg => {
+      svg
+        .attr('width', viewPortWidth)
+        .attr('height', height)
+        .append('g')
+        .call(xAxis)
+        .attr('stroke-width', 0)
+        .attr('transform', `translate(0, -2)`)
+    },
+    [legendValues]
+  );
+
+  return (
+    <svg
+      ref={ref}
+      style={{
+        height: height,
+        width: '100%',
+        marginTop: '0px',
+        marginRight: '0px',
+        marginLeft: '0px',
+      }}
+    ></svg>
+  );
+}
+
+export default D3HeaderComponent;
